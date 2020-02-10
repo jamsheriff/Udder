10315d433dc2ffb4626d5b2391b1d1098b409ce3
jamsheriff <jamsheriff@mail.ru>
2020-02-10 04:30:06 +0300

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Sherif Musa on 10.02.2020.
+//  Copyright © 2020 Sherif Musa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
